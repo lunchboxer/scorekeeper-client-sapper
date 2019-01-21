784716518e52123e07b2f5a5fe6d37170cc44fc3
James <metal.lunchbox@gmail.com>
2019-01-21 21:20:15 +0800

    back to localstorage  for auth token

--- src/utils/feathers.js
+++ src/utils/feathers.js
@@ -2,7 +2,6 @@ import io from 'socket.io-client'
 import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
-import { CookieStorage } from 'cookie-storage'
 import rx from 'feathers-reactive'
 
 const socket = io('http://192.168.1.9:3030')
@@ -10,6 +9,9 @@ const client = feathers()
 
 client.configure(socketio(socket))
 client.configure(rx({ idField: 'id' }))
-client.configure(auth({ storage: new CookieStorage() }))
-
+if (typeof window !== 'undefined') { // for ssr to not freak out
+  client.configure(auth({ storage: window.localStorage }))
+} else {
+  client.configure(auth())
+}
 export default client
