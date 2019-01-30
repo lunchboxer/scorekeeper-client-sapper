0086451aab84e9e9b2cd6826b084967cc91bd732
James <metal.lunchbox@gmail.com>
2019-01-30 11:47:47 +0800

    semesters route with CRUD

--- src/utils/store.js
+++ src/utils/store.js
@@ -12,11 +12,21 @@ const store = new StoreWithNotifications({
   activeStudents: [],
   recentPoint: null,
   authStatus: null,
-  viewer: null
+  viewer: null,
+  semesters: []
 })
 
 store.compute('activeGroup', ['session', 'groups'], (session, groups) => {
   return groups.find(group => group.id === session.studentGroupId)
+})
+store.compute('currentSemester', ['semesters'], (semesters) => {
+  if (!semesters || !semesters.length) return null
+  const now = new Date()
+  return semesters.filter(semester => {
+    const startDate = new Date(semester.startDate)
+    const endDate = new Date(semester.startDate)
+    return (startDate < now && now < endDate)
+  })
 })
 
 export default store
